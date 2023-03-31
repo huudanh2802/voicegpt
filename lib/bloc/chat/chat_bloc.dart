f7fa57ffd5dca9ff9a8eaf34545265630c1606d1
danhpch2802 <56262591+huudanh2802@users.noreply.github.com>
2023-03-31 21:26:18 +0700

    Update chat_bloc.dart

--- lib/bloc/chat/chat_bloc.dart
+++ lib/bloc/chat/chat_bloc.dart
@@ -10,6 +10,22 @@ class ChatBloc extends Bloc<ChatEvent, ChatState> {
   ChatBloc() : super(ChatInitial()) {
     on<ChatMessageEvent>((event, emit) {
       emit(ReceiveUserInput(userMessage: event.requestMessage));
+            try {
+        final request = ChatCompleteText(
+            model: kChatGptTurboModel,
+            maxToken: 3500,
+            messages: [
+              Map.of({"role": "user", "content": event.requestMessage})
+            ]);
+        final response = await openAI.onChatCompletion(request: request);
+        if (response != null) {
+          emit(RespondSuccess(respondMessage: response));
+        } else {
+          throw Exception("Fail to get message");
+        }
+      } catch (err) {
+        print(err);
+      }
     });
   }
 }
